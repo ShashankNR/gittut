a487f46086b0d02a383bfbc9a9e8e59826c79548
ShashankNR <shashankramu888@gmail.com>
2020-08-10 16:55:36 +0530

    mod: fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,17 @@
-def fibonacci(limit):
-    if limit <= 1:
-        return limit
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
-
-fibonacci(100)
+# Function for nth Fibonacci number 
+  
+def Fibonacci(n): 
+    if n < 0: 
+        print("Incorrect input") 
+    # First Fibonacci number is 0 
+    elif n == 0:  
+        return 0
+    # Second Fibonacci number is 1 
+    elif n == 1: 
+        return 1
+    else: 
+        return Fibonacci(n - 1)+Fibonacci(n - 2) 
+  
+# Driver Program 
+  
+print(Fibonacci(10)) 
